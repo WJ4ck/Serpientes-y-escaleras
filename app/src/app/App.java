66bca646bc76e86cb20148477f7e43a74db6a8d6
WJ4ck <122130280+WJ4ck@users.noreply.github.com>
2023-05-08 00:53:32 -0500

    Add files via upload

---
+++ app/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import app.consola.Consola;
+
+public class App {
+
+    public static void main(String[] args) {
+        Consola consola = new Consola();
+        consola.iniciarJuego();
+    }
+}
